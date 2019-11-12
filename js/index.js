35842dbaacc04194e910b41e24fea26d289d3b37
Rayan Santos Cavalcante e Silva <20192380014@ad.ifpb.edu.br>
2019-11-12 14:07:55 -0300

    .

--- js/index.js
+++ js/index.js
@@ -1,2 +1,23 @@
 $(() => {
+    var mainNavBar = $("#mainNavBar");
+    // mainNavBar.css({
+    //     "background-color":"transparent",
+    //     "box-shadow":"none"
+    // });
+    $(window).on('scroll', (e) => {
+        let ths = $(e.currentTarget);
+        let scrollValue = ths.scrollTop();
+        if(scrollValue > 80){
+            mainNavBar.css({
+                "background-color":"#333",
+                "box-shadow":"0 5px 3px #0003",
+            });
+        }
+        else{
+            mainNavBar.css({
+                "background-color":"transparent",
+                "box-shadow":"none"
+            });
+        }
+    })
 })
